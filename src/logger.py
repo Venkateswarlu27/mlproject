fc574cb61211f5904b379c6ee967dc4d264620d2
Venkateswarlu27 <venkateswarluthanneeru123@gmail.com>
2024-06-23 18:22:48 +0530

    commiting all changed loogers and exceptoim

--- src/logger.py
+++ src/logger.py
@@ -1,17 +1,17 @@
-import sys
 import logging
+import os
+from datetime import datetime
 
-def error_message_detail(error, error_detail):
-    _, _, exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occurred in Python script name [{0}] line number [{1}] error message [{2}]".format(file_name, exc_tb.tb_lineno, str(error))
-    return error_message
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-class CustomException(Exception):
-    def __init__(self, error, error_detail):
-        super().__init__(str(error))
-        self.error_message = error_message_detail(error, error_detail)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
-    def __str__(self):
-        return self.error_message
-    
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+
+
+)
