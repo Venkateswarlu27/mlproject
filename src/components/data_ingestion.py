bc1c200c0b5f40cc93f9ae128140ab78becee7b3
Venkateswarlu27 <venkateswarluthanneeru123@gmail.com>
2024-06-23 20:43:29 +0530

    pushing preprocessor pickle file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
+
 @dataclass##it is used to create the instance varialbes without init method it is not suitable if you are writing any functions in your classes
 class DataIngestionConfig:
     train_data_path:str=os.path.join('artifacts','train.csv')
@@ -43,4 +48,7 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
