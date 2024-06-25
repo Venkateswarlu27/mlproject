0e49e392b70441499e78a25c52639aa53a16a2ae
Venkateswarlu27 <venkateswarluthanneeru123@gmail.com>
2024-06-25 11:09:28 +0530

    saving the model file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,47 +8,45 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
 
-
-
-@dataclass##it is used to create the instance varialbes without init method it is not suitable if you are writing any functions in your classes
+@dataclass
 class DataIngestionConfig:
-    train_data_path:str=os.path.join('artifacts','train.csv')
-    test_data_path:str=os.path.join('artifacts','test.csv')
-    raw_data_path:str=os.path.join('artifacts','raw.csv')
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+    raw_data_path: str = os.path.join('artifacts', 'raw.csv')
 
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config=DataIngestionConfig()#when you clal this class with objects automatically all the path are stored in this variable
-
+        self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        logging.info("enter the data ingesiton method or component")
+        logging.info("Enter the data ingestion method or component")
         try:
-            df=pd.read_csv('data\\stud.csv')
-            logging.info('read dataset as dataframe')
+            df = pd.read_csv('data\\stud.csv')
+            logging.info('Read dataset as dataframe')
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
-            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
             logging.info("Train test split is initiated")
-            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
-            logging.info("ingesiton of data is completed")
+            logging.info("Ingestion of data is completed")
 
             return (
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
             )
         except Exception as e:
-            raise CustomException(e,sys)
-        
+            raise CustomException(e, sys)
 
-if __name__=="__main__":
-    obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
 
-    data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
