dd7cb3e2fd0f9edeb6b6ebf245fba1e52d53359f
Venkateswarlu27 <venkateswarluthanneeru123@gmail.com>
2024-06-25 13:30:11 +0530

    remodifying files inorder to execute in heroku

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 from src.pipeline.predict_pipleline import CustomData,PredictPipeline
-application=Flask(__name__)
+app=Flask(__name__)
 
-app=application
+
 
 
 @app.route('/')
